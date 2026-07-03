2810a1e4e5aa0f814d15b7851f9bc57eb2d17d68
allidamech <allidamech@gmail.com>
2026-07-03 18:39:38 +0300

    Update Android package ID for Google Play

--- android/app/src/main/java/com/rawaj/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rawaj.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.rawaj.app",
+  appId: "com.rawaj.marketplace",
   appName: "RAWAJ",
   webDir: ".output/public",
   server: {
