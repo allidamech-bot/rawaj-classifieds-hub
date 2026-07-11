8c86b1fcc45a0bd8ff7e955a8cea89a07809b6a4
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-11 04:34:26 +0300

    Load cohesive My Store review card styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -32,6 +32,7 @@ import listingsResultsCss from "../listings-results.css?url";
 import marketplaceDiscoveryCss from "../marketplace-discovery.css?url";
 import messagingSignatureCss from "../messaging-signature.css?url";
 import myStoreHeaderCss from "../my-store-header.css?url";
+import myStoreReviewCardCss from "../my-store-review-card.css?url";
 import offersSignatureCss from "../offers-signature.css?url";
 import sellerStorefrontFoundationCss from "../seller-storefront-foundation.css?url";
 import signatureCss from "../signature.css?url";
@@ -121,6 +122,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         { rel: "stylesheet", href: activityMoreFoundationCss },
         { rel: "stylesheet", href: sellerStorefrontFoundationCss },
         { rel: "stylesheet", href: myStoreHeaderCss },
+        { rel: "stylesheet", href: myStoreReviewCardCss },
         { rel: "stylesheet", href: offersSignatureCss },
         { rel: "stylesheet", href: listingStudioSignatureCss },
         { rel: "stylesheet", href: messagingSignatureCss },
