6955e5150faa67f4dd4d10473e5751730e321575
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-11 14:52:05 +0300

    Define explicit public marketplace column allowlists

---
+++ src/lib/api/public-fields.ts
@@ -0,0 +1,46 @@
+export const PUBLIC_LISTING_COLUMNS = [
+  "id",
+  "owner_id",
+  "category_id",
+  "subcategory_id",
+  "governorate_id",
+  "title",
+  "description",
+  "price",
+  "price_type",
+  "listing_condition",
+  "status",
+  "district_ar",
+  "contact_name",
+  "contact_options",
+  "details",
+  "is_featured",
+  "featured_until",
+  "published_at",
+  "archived_at",
+  "reserved_at",
+  "expires_at",
+  "renewed_at",
+  "expiry_days",
+  "created_at",
+  "updated_at",
+] as const;
+
+export const publicListingSelect = PUBLIC_LISTING_COLUMNS.join(",");
+
+export const PUBLIC_SELLER_REVIEW_COLUMNS = [
+  "id",
+  "seller_user_id",
+  "reviewer_user_id",
+  "related_listing_id",
+  "rating",
+  "comment",
+  "traits",
+  "status",
+  "seller_response",
+  "seller_response_updated_at",
+  "created_at",
+  "updated_at",
+] as const;
+
+export const publicSellerReviewSelect = PUBLIC_SELLER_REVIEW_COLUMNS.join(",");
