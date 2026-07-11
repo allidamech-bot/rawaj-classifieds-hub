da452b40e74018624cb379ac1c72ef380f88d3e0
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-11 14:55:56 +0300

    Fix JSON-LD escape literals

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -114,11 +114,11 @@ export function buildBreadcrumbStructuredData(items: BreadcrumbItem[]) {
 
 export function serializeJsonLd(data: unknown) {
   return JSON.stringify(data)
-    .replace(/</g, "\u003c")
-    .replace(/>/g, "\u003e")
-    .replace(/&/g, "\u0026")
-    .replace(/\u2028/g, "\u2028")
-    .replace(/\u2029/g, "\u2029");
+    .replace(/</g, "\\u003c")
+    .replace(/>/g, "\\u003e")
+    .replace(/&/g, "\\u0026")
+    .replace(/\u2028/g, "\\u2028")
+    .replace(/\u2029/g, "\\u2029");
 }
 
 export function jsonLdScript(data: unknown) {
