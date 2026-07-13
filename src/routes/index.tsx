b28d135d9fb343fb3def3cc7034e2ac7ee33d122
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-13 16:18:47 +0300

    Migrate home layout to canonical content rhythm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,7 +22,6 @@ export const Route = createFileRoute("/")({
       fetchPublicListings({}, null, 18),
       fetchPublicCategories(),
     ]);
-
     return {
       listings: listingsResult.ok ? listingsResult.data.items : [],
       categories: categoriesResult.ok ? categoriesResult.data : [],
@@ -62,7 +61,7 @@ function HomePage() {
       <AppHeader />
       <main className="rawaj-home-v3-main">
         <PageTransition>
-          <PageContainer className="rawaj-home-v3 py-3 sm:py-5 lg:py-7">
+          <PageContainer className="rawaj-home-v3 rawaj-content-stack py-3 sm:py-5 lg:py-7">
             <DiscoveryHero
               searchValue={searchValue}
               onSearchValueChange={setSearchValue}
