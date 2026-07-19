1e046e167d139a6e01fc414ada2411cffcc8e884
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-20 02:45:41 +0300

    feat: extend listing filters for governed attributes

---
+++ src/types/dynamic-listing-filters.d.ts
@@ -0,0 +1,14 @@
+import "@/lib/classifieds-types";
+
+declare module "@/lib/classifieds-types" {
+  interface ListingFilters {
+    attributeFilters?: Record<
+      string,
+      string | boolean | string[] | { min?: number; max?: number }
+    >;
+  }
+
+  interface PaginatedListingsResponse<T> {
+    totalCount?: number;
+  }
+}
