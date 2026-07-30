f5c88ba2b849db2d1f841c9ce6b6bff037c6d497
ALI M.DAAMISH <allidamech@gmail.com>
2026-07-30 18:35:48 +0300

    chore: keep Firebase configuration auth-only

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyCEx7XMl1vsLU3cvoDjrPav5WF54MFkn1U",
   authDomain: "project-af18fcaf-c46e-4ec5-93a.firebaseapp.com",
   projectId: "project-af18fcaf-c46e-4ec5-93a",
-  storageBucket: "project-af18fcaf-c46e-4ec5-93a.firebasestorage.app",
   messagingSenderId: "165848071823",
   appId: "1:165848071823:web:4969d4c035ae159ddabafa",
 };
